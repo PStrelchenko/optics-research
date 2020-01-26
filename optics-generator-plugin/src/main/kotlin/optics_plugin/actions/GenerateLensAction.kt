24a87e80c53755379904f5068d23279f7d47a643
Pavel Strelchenko <p.strelchenko@hh.ru>
2020-01-26 15:58:19 +0300

    [optics-generator-plugin] -- Action is enabled only for data classes

--- optics-generator-plugin/src/main/kotlin/optics_plugin/actions/GenerateLensAction.kt
+++ optics-generator-plugin/src/main/kotlin/optics_plugin/actions/GenerateLensAction.kt
@@ -2,11 +2,36 @@ package optics_plugin.actions
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.LangDataKeys
+import com.intellij.openapi.actionSystem.PlatformDataKeys
+import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.psi.KtClass
 
 class GenerateLensAction : AnAction() {
 
     override fun actionPerformed(e: AnActionEvent) {
+        // TODO: insert action logic here
+    }
 
+    override fun update(e: AnActionEvent) {
+        super.update(e)
+        e.presentation.isEnabled = getKtClassFromContext(e) != null
+    }
+
+
+    private fun getKtClassFromContext(e: AnActionEvent): KtClass? {
+        val currentPsiFile = e.getData(LangDataKeys.PSI_FILE)
+        val editor = e.getData(PlatformDataKeys.EDITOR)
+
+        if (currentPsiFile == null || editor == null) {
+            e.presentation.isEnabled = false
+            return null
+        }
+
+        val offset = editor.caretModel.offset
+        val psiElement = currentPsiFile.findElementAt(offset)
+
+        return PsiTreeUtil.getParentOfType(psiElement, KtClass::class.java)?.takeIf { it.isData() }
     }
 
 }
